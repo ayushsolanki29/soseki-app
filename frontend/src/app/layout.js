62f73543f7e3365efdc2bec1f78a920af87135e3
Ayush Solanki <ayushsolanki2901@gmail.com>
2026-07-21 17:29:33 +0530

    feat: initialize frontend project with Next.js, core dependencies, and global layout structure

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -5,7 +5,7 @@ import { Toaster } from "sonner";
 import { Suspense } from "react";
 import { BackgroundTracker } from "@/components/background-tracker";
 import { CookieConsent } from "@/components/cookie-consent";
-import Script from "next/script";
+import { GoogleTagManager } from '@next/third-parties/google';
 
 const plusJakartaSans = Plus_Jakarta_Sans({
   variable: "--font-sans",
@@ -28,24 +28,7 @@ export default function RootLayout({ children }) {
       suppressHydrationWarning
     >
       <body className="t-page-fade min-h-full flex flex-col overflow-x-hidden">
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-KS9248KF"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-        
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-KS9248KF');
-          `}
-        </Script>
+        <GoogleTagManager gtmId="GTM-KS9248KF" />
         <TooltipProvider>{children}</TooltipProvider>
         <Toaster richColors position="bottom-right" />
         <Suspense fallback={null}>
