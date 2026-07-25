9cfa76b5d075469864efade9e43b6b61e2ca1987
Ayush Solanki <ayushsolanki2901@gmail.com>
2026-07-25 16:37:19 +0530

    feat: implement sitemap.js for SEO indexation of public routes

--- frontend/src/app/sitemap.js
+++ frontend/src/app/sitemap.js
@@ -9,6 +9,18 @@ export default function sitemap() {
       priority: 1.0,
     },
     {
+      url: `${baseUrl}/login`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/setup-account`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    },
+    {
       url: `${baseUrl}/features`,
       lastModified: new Date(),
       changeFrequency: 'weekly',
